20c1326940295cc814609140f561872e01260925
nilaykumar07 <nilaykumar07@894212d4-b873-f137-25c2-ceafa4a5804e>
2011-01-22 21:28:07 +0000

    Implemented key listening for the InputManager and demonstrated its use in TestMap.

--- trunk/src/com/scriptrts/core/InputManager.java
+++ trunk/src/com/scriptrts/core/InputManager.java
@@ -5,24 +5,44 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.event.MouseInputListener;
 
 public class InputManager implements MouseInputListener, MouseWheelListener, KeyListener {
-    /* Singleton */
-    private final InputManager manager = new InputManager();
+    
+	/* Key listener states */
+	ArrayList<Integer> registeredKeyCodes = new ArrayList<Integer>();
+	HashMap<Integer, Boolean> keyCodeFlags = new HashMap<Integer, Boolean>();
+	
+	/* Singleton */
+    private final static InputManager manager = new InputManager();
 
     /* Prevent more than one from being created */
     private InputManager(){
         super();
     }
 
-    public InputManager getInputManager(){
+    public static InputManager getInputManager(){
         return manager;
     }
 
+    public void registerKeyCode(int code) {
+    	registeredKeyCodes.add(code);
+    	keyCodeFlags.put(code, false);
+    }
+    
+    public boolean getKeyCodeFlag(int code) {
+    	boolean flag = keyCodeFlags.get(code);
+    	keyCodeFlags.put(code, false);
+    	return flag;
+    }
+    
     /* Key listener */
-    public void keyPressed(KeyEvent key){}
+    public void keyPressed(KeyEvent key){
+    	keyCodeFlags.put(key.getKeyCode(), true);
+    }
     public void keyReleased(KeyEvent key){}
     public void keyTyped(KeyEvent key){}
 
--- trunk/src/com/scriptrts/core/map/TestMap.java
+++ trunk/src/com/scriptrts/core/map/TestMap.java
@@ -1,99 +1,113 @@
 package com.scriptrts.core.map;
 
-import java.awt.*;
-import javax.swing.JOptionPane;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import com.scriptrts.core.InputManager;
 
 public class TestMap extends JFrame {
 
-    int n = 1280, tilesize = 32, width=800, height=600;
-    int x,y;
-    String[][] terrain;
-    BufferedImage dirt, grass;
-    BufferedImage buf = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    Graphics2D ig = (Graphics2D) buf.getGraphics();
+	int n = 1280, tilesize = 32, width=800, height=600;
+	int x,y;
+	String[][] terrain;
+	BufferedImage dirt, grass;
+	BufferedImage buf = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	Graphics2D ig = (Graphics2D) buf.getGraphics();
+	InputManager manager = InputManager.getInputManager();
 
-    public TestMap() {
-        terrain = new String[n][n];
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if(Math.random() < 0.5)
-                    terrain[i][j] = "dirt";
-                else
-                    terrain[i][j] = "grass";
-            }
-        }
+	public TestMap() {
+		terrain = new String[n][n];
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if(Math.random() < 0.5)
+					terrain[i][j] = "dirt";
+				else
+					terrain[i][j] = "grass";
+			}
+		}
 
-        boolean fullscreen = JOptionPane.showConfirmDialog(null, "Enable Full Screen display?", "Fullscreen?", JOptionPane.YES_NO_OPTION) == 0;
-        if(fullscreen){
-            /* Disable resizing and decorations */
-            setUndecorated(true);
-            setResizable(false);
+		boolean fullscreen = JOptionPane.showConfirmDialog(null, "Enable Full Screen display?", "Fullscreen?", JOptionPane.YES_NO_OPTION) == 0;
+		if(fullscreen){
+			/* Disable resizing and decorations */
+			setUndecorated(true);
+			setResizable(false);
 
-            /* Switch to fullscreen and make window maximum size */
-            Toolkit toolkit = Toolkit.getDefaultToolkit();
-            Dimension scrnsize = toolkit.getScreenSize();
-            width = scrnsize.width;
-            height = scrnsize.height;
-            setSize(width, height);
+			/* Switch to fullscreen and make window maximum size */
+			Toolkit toolkit = Toolkit.getDefaultToolkit();
+			Dimension scrnsize = toolkit.getScreenSize();
+			width = scrnsize.width;
+			height = scrnsize.height;
+			setSize(width, height);
 
-            GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-            device.setFullScreenWindow(this);
-        }
+			GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+			device.setFullScreenWindow(this);
+		}
 
-        setSize(width, height);
+		setSize(width, height);
 
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-        add(new MapPanel());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		add(new MapPanel());
 
-        gameLoop();
-    }
+		gameLoop();
+	}
 
-    public static void main(String... args) {
-        TestMap app = new TestMap();
-    }
+	public static void main(String... args) {
+		TestMap app = new TestMap();
+	}
 
-    private void gameLoop() {
-        // load textures
-        dirt = grass = null;
-        try {
-            dirt = ImageIO.read(new File("resource/map/dirt32.png"));
-            grass = ImageIO.read(new File("resource/map/grass32.png"));
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-        x=y=0;
-        // initial map paint
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                ig.drawImage(terrain[i][j]=="dirt" ? dirt : grass, i*tilesize, j*tilesize, null);
-            }
-        }
-        while(true) {
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            repaint();
-        }
-    }
+	private void gameLoop() {
+		// load textures
+		dirt = grass = null;
+		try {
+			dirt = ImageIO.read(new File("resource/map/dirt32.png"));
+			grass = ImageIO.read(new File("resource/map/grass32.png"));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		x=y=0;
+		// set up key listeners
+		addKeyListener(manager);
+		manager.registerKeyCode(KeyEvent.VK_LEFT);
+		// initial map paint
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				ig.drawImage(terrain[i][j]=="dirt" ? dirt : grass, i*tilesize, j*tilesize, null);
+			}
+		}
+		while(true) {
+			// check for keystrokes
+			if(manager.getKeyCodeFlag(KeyEvent.VK_LEFT))
+				System.out.println("Left arrow key pressed.");
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			repaint();
+		}
+	}
 
-    private class MapPanel extends JPanel {
+	private class MapPanel extends JPanel {
 
-        @Override
-            protected void paintComponent(Graphics g) {
-                g.drawImage(buf, 0, 0, width, height, null);
-            }
+		@Override
+		protected void paintComponent(Graphics g) {
+			g.drawImage(buf, 0, 0, width, height, null);
+		}
 
-    }
+	}
 
 }
