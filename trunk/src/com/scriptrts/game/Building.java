bbad738b6ae845f9ea31faadee7e80a64ffa8360
workisnotavector <workisnotavector@894212d4-b873-f137-25c2-ceafa4a5804e>
2011-01-22 19:20:44 +0000

    Fleshed out Player a bit, added some isPassable functionality.

--- trunk/src/com/scriptrts/game/Building.java
+++ trunk/src/com/scriptrts/game/Building.java
@@ -2,4 +2,7 @@ package com.scriptrts.game;
 public abstract class Building extends Construct {
     private BuildingType type;
 
+    public boolean isPassable(Construct c){
+        return this.allegiance.equals(c.allegiance);
+    }
 }
--- trunk/src/com/scriptrts/game/Construct.java
+++ trunk/src/com/scriptrts/game/Construct.java
@@ -1,5 +1,13 @@
-public class Construct extends Entity {
-    private int hitpoints;
-    private Player allegiance;
+public abstract class Construct extends Entity {
+    protected int hitpoints;
+    protected Player allegiance;
+    protected boolean vulnerable;
 
+    /**
+     * Determines whether this is passable or transparent to c.
+     * Specific implementation depends on Construct type.
+     * @param c Construct attempting to determine whether this is passable.
+     * @return true if this is passable, false otherwise.
+     */
+    public abstract boolean isPassable(Construct c);
 }
