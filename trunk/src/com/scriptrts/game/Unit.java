7015be8d85e4f70b37ab61d14d42a43638871ae5
workisnotavector <workisnotavector@894212d4-b873-f137-25c2-ceafa4a5804e>
2011-01-22 20:53:52 +0000

    Got rid of UnitType and fixed Unit appropriately.

--- trunk/src/com/scriptrts/game/Unit.java
+++ trunk/src/com/scriptrts/game/Unit.java
@@ -1,6 +1,6 @@
 package com.scriptrts.game;
 public abstract class Unit extends Construct {
     private int speed;
-    private UnitType type;
+    
 
 }
--- trunk/src/com/scriptrts/game/UnitType.java
+++
@@ -1,5 +0,0 @@
-package com.scriptrts.game;
-
-public enum UnitType {
-
-}
