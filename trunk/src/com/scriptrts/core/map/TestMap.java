67e20038b20d621d75909fb3dc8bc99c24976fea
niftyion@gmail.com <niftyion@gmail.com@894212d4-b873-f137-25c2-ceafa4a5804e>
2011-01-22 21:03:28 +0000

    Fullscreen support

--- trunk/src/com/scriptrts/core/map/TestMap.java
+++ trunk/src/com/scriptrts/core/map/TestMap.java
@@ -1,6 +1,7 @@
 package com.scriptrts.core.map;
 
-import java.awt.Graphics;
+import java.awt.*;
+import javax.swing.JOptionPane;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -12,68 +13,87 @@ import javax.swing.JPanel;
 
 public class TestMap extends JFrame {
 
-	final int n = 1280, tilesize = 32, width=800, height=600;
-	int x,y;
-	String[][] terrain;
-	BufferedImage dirt, grass;
-	BufferedImage buf = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-	Graphics2D ig = (Graphics2D) buf.getGraphics();
-	
-	public TestMap() {
-		terrain = new String[n][n];
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				if(Math.random() < 0.5)
-					terrain[i][j] = "dirt";
-				else
-					terrain[i][j] = "grass";
-			}
-		}
-		setSize(width, height);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setVisible(true);
-		add(new MapPanel());
-		
-		gameLoop();
-	}
-	
-	public static void main(String... args) {
-		TestMap app = new TestMap();
-	}
-	
-	private void gameLoop() {
-		// load textures
-		dirt = grass = null;
-		try {
-			dirt = ImageIO.read(new File("resource/map/dirt32.png"));
-			grass = ImageIO.read(new File("resource/map/grass32.png"));
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		x=y=0;
-		// initial map paint
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				ig.drawImage(terrain[i][j]=="dirt" ? dirt : grass, i*tilesize, j*tilesize, null);
-			}
-		}
-		while(true) {
-			try {
-				Thread.sleep(50);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			repaint();
-		}
-	}
-	
-	private class MapPanel extends JPanel {
-		
-		@Override
-		protected void paintComponent(Graphics g) {
-			g.drawImage(buf, 0, 0, null);
-		}
-		
-	}
-	
+    int n = 1280, tilesize = 32, width=800, height=600;
+    int x,y;
+    String[][] terrain;
+    BufferedImage dirt, grass;
+    BufferedImage buf = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    Graphics2D ig = (Graphics2D) buf.getGraphics();
+
+    public TestMap() {
+        terrain = new String[n][n];
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(Math.random() < 0.5)
+                    terrain[i][j] = "dirt";
+                else
+                    terrain[i][j] = "grass";
+            }
+        }
+
+        boolean fullscreen = JOptionPane.showConfirmDialog(null, "Enable Full Screen display?", "Fullscreen?", JOptionPane.YES_NO_OPTION) == 0;
+        if(fullscreen){
+            /* Disable resizing and decorations */
+            setUndecorated(true);
+            setResizable(false);
+
+            /* Switch to fullscreen and make window maximum size */
+            Toolkit toolkit = Toolkit.getDefaultToolkit();
+            Dimension scrnsize = toolkit.getScreenSize();
+            width = scrnsize.width;
+            height = scrnsize.height;
+            setSize(width, height);
+
+            GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+            device.setFullScreenWindow(this);
+        }
+
+        setSize(width, height);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        add(new MapPanel());
+
+        gameLoop();
+    }
+
+    public static void main(String... args) {
+        TestMap app = new TestMap();
+    }
+
+    private void gameLoop() {
+        // load textures
+        dirt = grass = null;
+        try {
+            dirt = ImageIO.read(new File("resource/map/dirt32.png"));
+            grass = ImageIO.read(new File("resource/map/grass32.png"));
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        x=y=0;
+        // initial map paint
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                ig.drawImage(terrain[i][j]=="dirt" ? dirt : grass, i*tilesize, j*tilesize, null);
+            }
+        }
+        while(true) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            repaint();
+        }
+    }
+
+    private class MapPanel extends JPanel {
+
+        @Override
+            protected void paintComponent(Graphics g) {
+                g.drawImage(buf, 0, 0, width, height, null);
+            }
+
+    }
+
 }
