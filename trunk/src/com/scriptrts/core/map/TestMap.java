710a0f8a6f045f1c3a8833a155517ac275631ce5
nilaykumar07 <nilaykumar07@894212d4-b873-f137-25c2-ceafa4a5804e>
2011-01-22 16:28:24 +0000

    Changed the texture sizes to 32x32 for now. Started working on the graphics of the map testing.

--- trunk/src/com/scriptrts/core/map/TestMap.java
+++ trunk/src/com/scriptrts/core/map/TestMap.java
@@ -1,12 +1,42 @@
 package com.scriptrts.core.map;
 
-public class TestMap {
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TestMap extends JFrame {
 
 	final int n = 1280;
 	String[][] terrain;
 	
 	public TestMap() {
 		terrain = new String[n][n];
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if(Math.random() < 0.5)
+					terrain[i][j] = "dirt";
+				else
+					terrain[i][j] = "grass";
+			}
+		}
+		setSize(800, 600);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+			
+	}
+	
+	public static void main(String... args) {
+		TestMap app = new TestMap();
+	}
+	
+	private class MapPanel extends JPanel {
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+			
+		}
+		
 	}
 	
 }
