4843528c8fd3390b9eb32cb06b2c1eac51b5cb75
workisnotavector <workisnotavector@894212d4-b873-f137-25c2-ceafa4a5804e>
2011-01-23 04:03:41 +0000

    Generated getters and setters; minor revisions.

--- trunk/src/com/scriptrts/game/Entity.java
+++ trunk/src/com/scriptrts/game/Entity.java
@@ -8,6 +8,38 @@ public abstract class Entity {
     private int height;
     private int width;
     
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
     /**
      * Determines whether this is passable or transparent to c.
      * Specific implementation depends on Construct type.
--- trunk/src/com/scriptrts/game/Unit.java
+++ trunk/src/com/scriptrts/game/Unit.java
@@ -1,6 +1,75 @@
 package com.scriptrts.game;
 public abstract class Unit extends Construct {
-    private int speed;
+    protected double movementSpeed;
+    protected double attackSpeed;
+    // This is an int array because we may want different types of attack. 
+    //ex.: [land_attack air_attack] or [attack1 attack2] or something depending on plot
+    protected int[] damage;
     
+    protected Sprite sprite;
+    
+    // For weird-behaving units
+    protected boolean water;
+    protected boolean air;
+    
+    protected Direction direction;
+    
+    protected enum Direction{N, NE, E, SE, S, SW, W, NW}
+
+    public double getMovementSpeed() {
+        return movementSpeed;
+    }
+
+    public void setMovementSpeed(double movementSpeed) {
+        this.movementSpeed = movementSpeed;
+    }
+
+    public double getAttackSpeed() {
+        return attackSpeed;
+    }
+
+    public void setAttackSpeed(double attackSpeed) {
+        this.attackSpeed = attackSpeed;
+    }
+
+    public int[] getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int[] damage) {
+        this.damage = damage;
+    }
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+    }
+
+    public boolean isWater() {
+        return water;
+    }
+
+    public void setWater(boolean water) {
+        this.water = water;
+    }
+
+    public boolean isAir() {
+        return air;
+    }
+
+    public void setAir(boolean air) {
+        this.air = air;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    };
 
 }
