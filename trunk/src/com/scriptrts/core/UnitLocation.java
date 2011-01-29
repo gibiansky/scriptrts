424e8f8ebbd1c07f37a6a02a4634e9eac917da9e
niftyion@gmail.com <niftyion@gmail.com@894212d4-b873-f137-25c2-ceafa4a5804e>
2011-01-29 16:07:54 +0000

    Moving unit #1

--- trunk/src/com/scriptrts/core/UnitLocation.java
+++ trunk/src/com/scriptrts/core/UnitLocation.java
@@ -17,23 +17,23 @@ public enum UnitLocation {
     public UnitLocation move(Direction d){
         final UnitLocation[][] moveMapping = {
             /* Northwest */
-            {},
+            {Southwest, South, North, Center, West, East, Northeast, Southeast},
             /* West */
-            {},
+            {Northwest, North, Center, South, West, Southwest, East, Northeast},
             /* North */
-            {},
+            {South, Southeast, Northeast, East, Center, West, Northwest, Southwest},
             /* Southwest */
-            {},
+            {West, Center, South, North, Northwest, Northeast, Southeast, East},
             /* Center */
-            {},
+            {North, Northeast, East, Southeast, South, Southwest, West, Northwest},
             /* Northeast */
-            {},
+            {Southeast, Southwest, Northwest, West, East, Center, North, South},
             /* South */
-            {},
+            {Center, East, Southeast, Northeast, North, Northwest, Southwest, West},
             /* East */
-            {},
+            {Northeast, Northwest, West, Southwest, Southeast, South, Center, North},
             /* Southeast */
-            {}
+            {East, West, Southwest, Northwest, Northeast, North, South, Center}
         };
 
         return moveMapping[ordinal()][d.ordinal()];
--- trunk/src/com/scriptrts/game/UnitGrid.java
+++ trunk/src/com/scriptrts/game/UnitGrid.java
@@ -1,5 +1,7 @@
 package com.scriptrts.game;
 
+import java.awt.Point;
+import com.scriptrts.core.UnitLocation;
 
 public class UnitGrid {
 
@@ -17,56 +19,79 @@ public class UnitGrid {
 
         /* Figure out new unit location */
         UnitLocation newLoc = loc.move(d);
+        int i = (int) unit.getY();
         
         /* Figure out if it's in a new tile */
-        int tileshiftX = 0, tileshiftY = 0;
-        if(loc == UnitLocation.Northwest && d == Direction.Northwest){
-            tileshiftY = -2;
-        }
-        if(loc == UnitLocation.North && d == Direction.North){
-            tileshiftY = -1;
-            tileshiftX = (i % 2 == 0) ? 0 : 1;
-        }
-        if(loc == UnitLocation.Northeast && d == Direction.Northeast){
-            tileshiftX = 1;
-        }
-        if(loc == UnitLocation.East && d == Direction.East){
-            tileshiftY = 1;
-            tileshiftX = (i % 2 == 0) ? 0 : -1;
-        }
-        if(loc == UnitLocation.Southeast && d == Direction.Southeast){
-            tileshiftY = 2;
-        }
-        if(loc == UnitLocation.South && d == Direction.South){
-            tileshiftY = 1;
-            tileshiftX = (i % 2 == 0) ? -1 : 0;
-        }
-        if(loc == UnitLocation.Southwest && d == Direction.Southwest){
-            tileshiftX = -1;
-        }
-        if(loc == UnitLocation.West && d == Direction.West){
-            tileshiftY = -1;
-            tileshiftX = (i % 2 == 0) ? -1 : 0;
-        }
+        final Point north       = new Point(-1, i % 2 == 0 ? 0 : 1);
+        final Point northeast   = new Point(0, 1);
+        final Point east        = new Point(1, i % 2 == 0 ? 0 : 1);
+        final Point southeast   = new Point(2, 0);
+        final Point south       = new Point(1, i % 2 == 0 ? -1 : 0);
+        final Point southwest   = new Point(0, -1);
+        final Point west        = new Point(-1, i % 2 == 0 ? -1 : 0);
+        final Point northwest   = new Point(-2, 0);
+        final Point none        = new Point(0, 0);
+        final Point[][] tileshifts = {
+            /* Northwest */
+            {north, north, none, none, none, west, west, northwest},
+            /* West */
+            {none, none, none, none, none, west, west, west},
+            /* North */
+            {north, north, none, none, none, none, none, north},
+            /* Southwest */
+            {none, none, none, south, south, southwest, west, west},
+            /* Center */
+            {none, none, none, none, none, none, none, none},
+            /* Northeast */
+            {north, northeast, east, east, none, none, none, north},
+            /* South */
+            {none, none, none, south, south, south, none, none},
+            /* East */
+            {none, east, east, east, none, none, none, none},
+            /* Southeast */
+            {none, east, east, southeast, south, south, none, none}
+        };
+
+        Point shift = tileshifts[loc.ordinal()][d.ordinal()];
+        int tileshiftI = (int) (shift.getX());
+        int tileshiftJ = (int) (shift.getY());
 
         int originalX = unit.getX();
         int originalY = unit.getY();
 
+        /* Set new unit location (before moving tiles, so that the unit placer knows where to put the unit) */
+        unit.setUnitLocation(newLoc);
+
         /* If we're moving between tiles */
-        if(tileshiftX != 0 || tileshiftY != 0){
-            int finalX = originalX + tileshiftX;
-            int finalY = originalY + tileshiftY;
+        if(tileshiftI != 0 || tileshiftJ != 0){
+            int finalX = originalX + tileshiftJ;
+            int finalY = originalY + tileshiftI;
             
             /* Remove unit from current tile placement */
-            unitGrid[originalX][originalY].removeUnit(unit);
-            if(unitGrid[originalX][originalY].isEmpty())
-                unitGrid[originalX][originalY] = null;
+            unitGrid[originalY][originalX].removeUnit(unit);
+            if(unitGrid[originalY][originalX].isEmpty())
+                unitGrid[originalY][originalX] = null;
 
             /* Add unit to new tile placement */
-            if(unitGrid[finalX][finalY] == null)
-                unitGrid[finalX][finalY] = new UnitTile();
+            if(unitGrid[finalY][finalX] == null)
+                unitGrid[finalY][finalX] = new UnitTile();
 
-            unitGrid[finalX][finalY].addUnit(unit);
+            unitGrid[finalY][finalX].addUnit(unit);
+
+            unit.setX(finalX);
+            unit.setY(finalY);
         }
+
+        /* If we're not moving between tiles, just rearrange unit slots in the current tile to reflect the movement */
+        else {
+            UnitTile tile = unitGrid[originalY][originalX];
+            for(int k = 0; k < tile.units.length; k++){
+                if(k == newLoc.ordinal())
+                    tile.units[k] = unit;
+                else if(tile.units[k] == unit)
+                    tile.units[k] = null;
+            }
+        }
+
     }
 }
