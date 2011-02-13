ba274310182e265a47da2479b0b9aa177e5b1380
niftyion@gmail.com <niftyion@gmail.com@894212d4-b873-f137-25c2-ceafa4a5804e>
2011-02-13 02:01:07 +0000

    deleted resources

--- trunk/resource/map/texture.sh
+++
@@ -1,8 +0,0 @@
-for f in *.jpg
-do
-     echo "Processing $f"
-
-     #  Convert file to png texture
-     pngname=`echo $f | sed 's/\.jpg$//g'`
-     convert "$f" -background "rgba(0, 0, 0, 0)" -rotate '45' -resize "100%x50%" "$pngname.png"
-done
