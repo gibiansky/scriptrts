00b544c9e0ba17149386879cb5d4204952d9ac1e
nilaykumar07 <nilaykumar07@894212d4-b873-f137-25c2-ceafa4a5804e>
2011-02-26 18:47:04 +0000

    Some work on console; started graphical testbed for pathfinding algorithms.

--- trunk/src/com/scriptrts/core/ConsoleTester.java
+++ trunk/src/com/scriptrts/core/ConsoleTester.java
@@ -1,8 +1,16 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+package com.scriptrts.core;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class ConsoleTester extends JFrame implements KeyListener {
 
@@ -10,7 +18,15 @@ public class ConsoleTester extends JFrame implements KeyListener {
 	private MyPanel panel;
 	private Timer timer;
 
+	/* Whether or not the console is currently showing */
 	private boolean isConsoleDown;
+	/* The text currently entered */
+	private String buffer;
+	/* The text previously entered */
+	private ArrayList<String> cmdHistory;
+	/* The previous output */
+	private ArrayList<String> outputHistory;
+
 
 	public static void main(String... args) {
 		ConsoleTester test = new ConsoleTester();
@@ -37,15 +53,22 @@ public class ConsoleTester extends JFrame implements KeyListener {
 			g.setColor(Color.white);
 			g.fillRect(0, 0, getWidth(), getHeight());
 			if(isConsoleDown) {
-				g.setColor(new Color(0, 0, 0, 175));
+				g.setColor(new Color(0, 0, 0, 220));
 				g.fillRect(0, 0, getWidth(), getHeight() / 5);
 			}
 		}
 	}
 
 	public void keyPressed(KeyEvent evt) {
-		if(evt.getKeyCode() == KeyEvent.VK_BACK_QUOTE)
+		if(evt.getKeyCode() == KeyEvent.VK_BACK_QUOTE) {
 			isConsoleDown = ! isConsoleDown;
+			return;
+		}
+		int asciiVal = (int) evt.getKeyChar();
+		if(isConsoleDown && asciiVal <= 125 && asciiVal >= 32) {
+			String charEntered = evt.getKeyChar() + "";
+			System.out.println(charEntered);
+		}
 	}
 
 	public void keyReleased(KeyEvent evt) {
