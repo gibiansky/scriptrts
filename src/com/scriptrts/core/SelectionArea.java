2b200f4ccbb130383a7b62ebb4d37df1292b6ac4
niftyion@gmail.com <niftyion@gmail.com@894212d4-b873-f137-25c2-ceafa4a5804e>
2011-03-05 06:32:18 +0000

    Added single unit view (pretty picturePrevented InputManager from remembering mouse clicks for too long (over .2 of a second).

--- src/com/scriptrts/core/SelectionArea.java
+++ src/com/scriptrts/core/SelectionArea.java
@@ -91,24 +91,36 @@ public class SelectionArea extends JPanel {
         image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         Graphics2D graphics = (Graphics2D) image.getGraphics();
         
+        
         ArrayList<SimpleUnit> units = Selection.current().getObjects();
 
-        /* Unit size */
-        int size = 60;
-        int verticalMargin = 5;
-        int horizontalMargin = calculateMargin(units.size());
-        int rows = height / (size+verticalMargin);
-        int unitsPerRow = width / (size+horizontalMargin);
+        /* For single unit select, draw unit info screen */
+        if(units.size() == 1){
+            SimpleUnit unit = units.get(0);
+            int size = 180;
+            int x = 20, y = 20;
+            graphics.drawImage(unit.getArt(), x, y, size, size, null);
+        } 
+        
+        /* Draw all units for multiple unit select */
+        else {
+            /* Unit size */
+            int size = 60;
+            int verticalMargin = 5;
+            int horizontalMargin = calculateMargin(units.size(), size);
+            int rows = height / (size+verticalMargin);
+            int unitsPerRow = width / (size+horizontalMargin);
 
-        /* Draw each unit */
-        for(int i = 0; i < units.size(); i++){
-            int col = i % unitsPerRow;
-            int x = col * size + (col) * horizontalMargin;
+            /* Draw each unit */
+            for(int i = 0; i < units.size(); i++){
+                int col = i % unitsPerRow;
+                int x = col * size + (col) * horizontalMargin + 5;
 
-            int row = i / unitsPerRow;
-            int y = row * size + (row+1) * verticalMargin;
+                int row = i / unitsPerRow;
+                int y = row * size + (row+1) * verticalMargin;
 
-            graphics.drawImage(units.get(i).getArt(), x, y, size, size, null);
+                graphics.drawImage(units.get(i).getArt(), x, y, size, size, null);
+            }
         }
     }
 
@@ -116,22 +128,31 @@ public class SelectionArea extends JPanel {
      * Calculate how much space there should be between unit icons
      * @return pixels between units; can be negative to make them stack
      */
-    private int calculateMargin(int units){
-        if(units <= 18)
-            return 6;
-        else if(units <= 24)
-            return -10;
-        else if(units <= 30)
-            return -26;
-        else 
-            return -42;
+    private int calculateMargin(int units, int size){
+        final int[] margins = {
+            6, -10, -26, -42, -54, -58
+        };
+        for(int margin : margins){
+            int unitsPerRow = width / (size+margin);
+            int rowsFilled = units / unitsPerRow;
+
+            /* If it doesn't fit, reject it */
+            if(rowsFilled > 3 || (rowsFilled == 3 && units % unitsPerRow != 0)) 
+                continue;
+
+            /* If it works, use it */
+            else
+                return margin;
+        }
+
+        return -58;
     }
 
     /**
      * Resize based on window
      */
     public void setWidth(int w){
-        this.width = w;
+        this.width = w - 5;
     }
 
     /**
