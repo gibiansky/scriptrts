59b5b8008b2f405bc44e8e17c94ee23a353d672c
trzhang@gmail.com <trzhang@gmail.com@894212d4-b873-f137-25c2-ceafa4a5804e>
2011-03-06 03:23:09 +0000

    Changed pathfinding from map to unit grid

--- src/com/scriptrts/core/Map.java
+++ src/com/scriptrts/core/Map.java
@@ -66,47 +66,7 @@ public class Map {
 	 */
 	public int getN() {
 		return n;
-	}
-	
-	/**
-     * Check whether the map contains a given point
-	 * @return if the map contains a given point
-	 */
-	
-	public boolean contains(int x, int y){
-		if(0 <= x && x <= n-1 && 0 <= y && y <= n-1)
-			return true;
-		return false;
-	}
-	
-	/**
-     * Get the neighbors of a point
-	 * @return the neighbors of a given point
-	 */
-	public int[][] getNeighbors(int x, int y){
-		int[][] neighbors;
-		int count = 0;
-		if(!this.contains(x,y))
-			return null;
-		if(x == 0 || x == n - 1)
-			if(y == 0 || y == n - 1)
-				neighbors = new int[3][2];
-			else
-				neighbors = new int[5][2];
-		else if(y == 0 || y == n - 1)
-			neighbors = new int[5][2];
-		else
-			neighbors = new int[8][2];
-		for(int i = x - 1; i <= x + 1; i++)
-			for(int j = y - 1; j <= y + 1; j++){
-				if(this.contains(i,j) && !(x == i && y == j)){
-					neighbors[count][0] = i;
-					neighbors[count][1] = j;
-					count++;
-				}
-			}
-		return neighbors;
-	}
+	}	
 	
 	/**
 	 * Generate random map using diamond-square algorithm described
