71fdd6cc90247078816e3fbfec0418392200d3f1
workisnotavector <workisnotavector@894212d4-b873-f137-25c2-ceafa4a5804e>
2011-03-20 18:41:14 +0000

    Major refactoring: changed SimpleUnit to MapObject and made the Entity/Construct/Unit hierarchy actually be used. Also changed some packaging. util edition

--- trunk/src/com/scriptrts/util/ResourceManager.java
+++ trunk/src/com/scriptrts/util/ResourceManager.java
@@ -1,5 +1,6 @@
 package com.scriptrts.util;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
@@ -8,7 +9,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.awt.Color;
 
 import javax.imageio.ImageIO;
 
