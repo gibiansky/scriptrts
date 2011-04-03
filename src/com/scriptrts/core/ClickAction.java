f0c9eb2252ba7771346e693d659cce150b8e21db
workisnotavector <workisnotavector@894212d4-b873-f137-25c2-ceafa4a5804e>
2011-04-03 16:37:44 +0000

    Change ClickAction to abstract class.

--- src/com/scriptrts/core/ClickAction.java
+++ src/com/scriptrts/core/ClickAction.java
@@ -5,17 +5,23 @@ import java.awt.Graphics;
 /**
  * Action that should be taken when the map is next clicked
  */
-public interface ClickAction {
+public abstract class ClickAction {
     /**
      * Method called when map is clicked
      * @param x x coordinate on map
      * @param y y coordinate on map
      */
-    public void click(int x, int y);
+    public abstract void click(int x, int y);
 
     /**
      * Draws cursor for the action
      * @param graphics graphics handle on the screen
      */
-    public void paintCursor(Graphics graphics);
+    public void paintCursor(Graphics graphics){}
+    
+    /**
+     * Does this action have a custom cursor?
+     * @return hasCursor whether this action has a custom cursor
+     */
+    public boolean hasCursor(){return false;}
 }
