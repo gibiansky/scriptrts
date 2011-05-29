307e9f8e9c12fd77e4b225f9c7da2df8db75fd81
nilaykumar07@gmail.com <nilaykumar07@gmail.com@894212d4-b873-f137-25c2-ceafa4a5804e>
2011-05-29 18:27:45 +0000

    Started work on AI

---
+++ trunk/src/com/scriptrts/ai/Agent.java
@@ -0,0 +1,39 @@
+package com.scriptrts.ai;
+
+public class Agent {
+
+	private double x, y, vx, vy;
+
+	public double getVy() {
+		return vy;
+	}
+
+	public void setVy(double vy) {
+		this.vy = vy;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public void setVx(double vx) {
+		this.vx = vx;
+	}
+
+	public double getVx() {
+		return vx;
+	}
+	
+}
